18f0fb42a6a68556a84881a97113297f883392d6
Marius Daniel Boncica <102974083+UnicoAI@users.noreply.github.com>
2023-01-27 10:23:25 +0000

    Initial commit

---
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleAppProject.App01
+{
+    /// <summary>
+    /// Please describe the main features of this App
+    /// </summary>
+    /// <author>
+    /// Derek version 0.1
+    /// </author>
+    public class DistanceConverter
+    {
+    }
+}
---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleAppProject.App01;
+using ConsoleAppProject.App03;
+using ConsoleAppProject.Helpers;
+using System;
+
+namespace ConsoleAppProject
+{
+    /// <summary>
+    /// The main method in this class is called first
+    /// when the application is started.  It will be used
+    /// to start App01 to App05 for CO453 CW1
+    /// 
+    /// This Project has been modified by:
+    /// Derek Peacock 05/02/2022
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.WriteLine(" =================================================");
+            Console.WriteLine("    BNU CO453 Applications Programming 2021-2022! ");
+            Console.WriteLine("        by Derek                                  ");
+            Console.WriteLine(" =================================================");
+            Console.WriteLine();
+
+            DistanceConverter converter = new DistanceConverter();
+            converter.Run();
+        }
+    }
+}
