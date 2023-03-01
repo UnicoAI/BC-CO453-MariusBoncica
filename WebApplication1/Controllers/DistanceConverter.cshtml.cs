fb3be12cc340ad25c288f45f53ad3265a3e99c5a
Marius Daniel Boncica <102974083+UnicoAI@users.noreply.github.com>
2023-03-01 23:14:06 +0000

    Web app asp.net

---
+++ WebApplication1/Controllers/DistanceConverter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Controllers
+{
+    public class DistanceConverterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
